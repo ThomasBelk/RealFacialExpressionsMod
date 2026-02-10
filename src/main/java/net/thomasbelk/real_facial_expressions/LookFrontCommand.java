f9ecfc62127e764b877ea4f03e9d6be14d7c390f
ThomasBelk <thomasjb03@gmail.com>
2026-02-10 09:33:11 -0500

    InitialCommit: Includes simple command to look at your character from the front. Meant to be used for facial animation debuging.

---
+++ src/main/java/net/thomasbelk/real_facial_expressions/LookFrontCommand.java
@@ -0,0 +1,48 @@
+package net.thomasbelk.real_facial_expressions;
+
+import com.hypixel.hytale.component.Ref;
+import com.hypixel.hytale.component.Store;
+import com.hypixel.hytale.protocol.*;
+import com.hypixel.hytale.protocol.packets.camera.SetServerCamera;
+import com.hypixel.hytale.server.core.command.system.CommandContext;
+import com.hypixel.hytale.server.core.command.system.basecommands.AbstractPlayerCommand;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
+import com.hypixel.hytale.server.core.universe.world.World;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
+import org.jspecify.annotations.NonNull;
+import com.hypixel.hytale.protocol.ServerCameraSettings;
+
+/**
+ * This is an example command that will simply print the name of the plugin in chat when used.
+ */
+public class LookFrontCommand extends AbstractPlayerCommand {
+    private static final String NAME = "lookfront";
+    private static final String DESC = "Changes camera view to look at front of character";
+    public LookFrontCommand() {
+        super(NAME, DESC);
+    }
+
+    @Override
+    protected void execute(@NonNull CommandContext commandContext, @NonNull Store<EntityStore> store, @NonNull Ref<EntityStore> ref, @NonNull PlayerRef playerRef, @NonNull World world) {
+        playerRef.getPacketHandler().writeNoCache(new SetServerCamera(ClientCameraView.Custom, true, createServerCameraSettings()));
+    }
+
+    public static ServerCameraSettings createServerCameraSettings() {
+        ServerCameraSettings cameraSettings = new ServerCameraSettings();
+        cameraSettings.isFirstPerson = false;
+        cameraSettings.displayCursor = false;
+        cameraSettings.sendMouseMotion = false;
+
+        cameraSettings.positionLerpSpeed = 0.2f;
+        cameraSettings.rotationLerpSpeed = 0.2f;
+        cameraSettings.distance = 2.0f;
+
+        cameraSettings.rotationType = RotationType.AttachedToPlusOffset;
+        cameraSettings.movementForceRotationType = MovementForceRotationType.AttachedToHead;
+        cameraSettings.eyeOffset = true;
+        cameraSettings.positionDistanceOffsetType = PositionDistanceOffsetType.DistanceOffset;
+        cameraSettings.rotationOffset = new Direction((float) Math.PI, 0, 0);
+        cameraSettings.planeNormal = new Vector3f(0.0f, 1.0f, 0.0f);
+        return cameraSettings;
+    }
+}
---
+++ src/main/java/net/thomasbelk/real_facial_expressions/RealFacialExpressionsPlugin.java
@@ -0,0 +1,25 @@
+package net.thomasbelk.real_facial_expressions;
+
+import com.hypixel.hytale.logger.HytaleLogger;
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class RealFacialExpressionsPlugin extends JavaPlugin {
+    private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
+    ConcurrentLinkedQueue<FacePacket> faceQueue;
+    Thread listenerThread;
+    FacePacketListener listener;
+
+    public RealFacialExpressionsPlugin(JavaPluginInit init) {
+        super(init);
+        LOGGER.atInfo().log("Hello from %s version %s", this.getName(), this.getManifest().getVersion().toString());
+    }
+
+
+    @Override
+    protected void setup() {
+        this.getCommandRegistry().registerCommand(new LookFrontCommand());
+    }
+}
