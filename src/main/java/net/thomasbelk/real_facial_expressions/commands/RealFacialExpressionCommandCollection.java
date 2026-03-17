ba5d83386fcdeb9c5e620752376c082e80dee17b
ThomasBelk <thomasjb03@gmail.com>
2026-03-17 16:43:17 -0400

    Bugfix where you needed op to /faceid, /resetFaceId, and /faceSettings. Now you don't need any permissions.

--- src/main/java/net/thomasbelk/real_facial_expressions/commands/RealFacialExpressionCommandCollection.java
+++ src/main/java/net/thomasbelk/real_facial_expressions/commands/RealFacialExpressionCommandCollection.java
@@ -1,10 +1,12 @@
 package net.thomasbelk.real_facial_expressions.commands;
 
+import com.hypixel.hytale.protocol.GameMode;
 import com.hypixel.hytale.server.core.command.system.basecommands.AbstractCommandCollection;
 
 public class RealFacialExpressionCommandCollection extends AbstractCommandCollection {
     public RealFacialExpressionCommandCollection() {
         super("rfe", "Real Facial Expression Commands");
+        this.setPermissionGroup(GameMode.Adventure);
         addSubCommand(new DisplayFaceIdCommand());
         addSubCommand(new ResetFaceIdCommand());
         addSubCommand(new FaceSettingsCommand());
